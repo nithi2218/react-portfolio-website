3ff7aebab0486adc7deb5d12b57c3439e05564e7
Nithyadevi <nithi2218@gmail.com>
2022-06-10 11:38:09 +1000

    README

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,7 @@ const About = () => {
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
     <p className='text-xl sm:text-3xl font-bold text-[#ccd6f6]'>Nithyadevi Sakthivel</p>
     <p className='text-xl text-[#ccd6f6] h-[100px] p-7'>I'm a graduate front-end web developer located in Sydney. I'm looking for a job opportunity as I have serious
-passion for creative Web developing.
-Well-Organised person, Problem solver with high attention to details.
+passion for creative Web developing. Well-Organised person, Problem solver with high attention to details.
 Interested in the entire frontend spectrum and looking for ambitious project with positive people.</p>
 </div>
   
