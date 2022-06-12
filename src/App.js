619fc242ec95dc2a9ef27c0c30a384ef40099be8
Nithyadevi <nithi2218@gmail.com>
2022-06-12 23:16:41 +1000

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react"
-
 import Home from "./pages/Home"
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <>
-   
-    <Routes>
-    
-    <Route index element={<Home />} />
+  <>
+  <Navbar />
+<div>
+  <Routes>
+    <Route path='/' element={<Home />} />
     <Route path='/about' element={<About />} />
     <Route path='/skills' element={<Skills />} />
     <Route path='/contact' element={<Contact />} />
     
-    </Routes>
-   </>
+  </Routes>
+  </div>
+  </>
   )
 }
 
